9473b9fea46d9575b3b605a67420fa1c9b8f02f6
bsusername <jim.my.garzon@gmail.com>
2014-06-14 18:01:34 -0400

    add deliveries in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,4 +47,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
 end
