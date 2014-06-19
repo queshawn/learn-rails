58124f5869f4f31a7631d5733a1ca81fc64c1cbd
queshawn <queshawn.hinton@gmail.com>
2014-06-19 08:19:31 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-<<<<<<< HEAD
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_record/railtie"
@@ -9,9 +8,9 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-=======
+
 require 'rails/all'
->>>>>>> 6577dbf159860ee184ff9330f3fcf53caff39652
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
